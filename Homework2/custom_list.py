56e5f21759174c80778e9637bdaedff07c6ba531
Aleksey-Korneev <AlekseyKorneev.CMC@yandex.ru>
2021-04-28 19:52:14 +0300

    Created CustomList class

--- Homework2/custom_list.py
+++ Homework2/custom_list.py
@@ -1,2 +1,16 @@
-def custom_list(list):
-    pass
+from itertools import zip_longest
+
+class CustomList(list):
+    """
+    Custom list supporting addition, subtraction and comparison.
+    """
+    def __add__(self, other):
+        return CustomList(map(lambda item : item[0] + item[1], \
+            zip_longest(self, other, fillvalue = 0)))
+
+    def __sub__(self, other):
+        return CustomList(map(lambda item : item[0] - item[1], \
+            zip_longest(self, other, fillvalue = 0)))
+
+    def __lt__(self, other):
+        return sum(self) < sum(other)
