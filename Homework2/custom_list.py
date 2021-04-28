d704e76adcae79e11ce56576d47bb9b80c3bfcc2
Aleksey-Korneev <AlekseyKorneev.CMC@yandex.ru>
2021-04-28 20:18:42 +0300

    Implemented comparison magic methods in custom_list.py. Created custom_meta.py

--- Homework2/custom_list.py
+++ Homework2/custom_list.py
@@ -14,3 +14,18 @@ class CustomList(list):
 
     def __lt__(self, other):
         return sum(self) < sum(other)
+
+    def __le__(self, other):
+        return sum(self) <= sum(other)
+
+    def __gt__(self, other):
+        return sum(self) > sum(other)
+
+    def __ge__(self, other):
+        return sum(self) >= sum(other)
+
+    def __eq__(self, other):
+        return sum(self) == sum(other)
+
+    def __ne__(self, other):
+        return sum(self) != sum(other)
